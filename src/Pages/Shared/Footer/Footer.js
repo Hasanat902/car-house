344a5f30c2609a69cd448750d852ac492a0482ee
Hasanat902 <abulhasanat902@gmail.com>
2023-01-08 18:53:01 +0600

    navbar added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Footer = () => {
+
+    const today = new Date();
+    const year = today.getFullYear();
+
     return (
-        <footer>
-            <p><small>Copyright @Car house</small></p>
+        <footer className='text-center mt-5'>
+            <p><small>Copyright @ Car house {year}</small></p>
         </footer>
     );
 };
