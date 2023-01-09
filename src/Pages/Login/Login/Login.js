aea9442643380a02a9934d64c5472d161e9f36ee
Hasanat902 <abulhasanat902@gmail.com>
2023-01-09 14:56:42 +0600

    register form created

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,15 +1,10 @@
 import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
-    const navigate = useNavigate();
-
-    const navigateRegister = () => {
-        navigate('/register');
-    }
 
     const handleSubmit = event => {
         event.preventDefault();
@@ -25,7 +20,7 @@ const Login = () => {
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+          <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -33,7 +28,7 @@ const Login = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control ref={passwordRef} type="password" placeholder="Password" />
+          <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
@@ -42,7 +37,7 @@ const Login = () => {
           Submit
         </Button>
       </Form>
-      <p>New to Car House? <span className="text-primary" onClick={navigateRegister}>Please Register</span></p>
+      <p>New to Car House? <Link to={'/register'} className="text-primary text-decoration-none">Please Register</Link></p>
     </div>
   );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Register.css';
 
 const Register = () => {
+
+    const handleRegister = event => {
+        event.preventDefault();
+    }
+
     return (
-        <div>
-            <h2>Please Register</h2>
+        <div className='register-form'>
+            <h2 className='text-primary text-center'>Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" id="" placeholder='Your name' />
+                <input type="email" name="email" id="" placeholder='Email' />
+                <input type="password" name="password" id="" placeholder='Password' />
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account? <Link to={'/login'} className="text-primary text-decoration-none">Please Login</Link></p>
         </div>
     );
 };
